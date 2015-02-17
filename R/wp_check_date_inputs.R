52c85ce5ddd41367a2467d975b91210c117f67aa
petermeissner <retep.meissner@gmail.com>
2015-02-17 14:45:57 +0100

    wp_check_date_inputs: overhaul
    - checks for-date AND to-date for too small values (no date before 2007)
    - checks for-date AND to-date for too large values (no data after today)
    - checks if: from <= to

--- R/wp_check_date_inputs.R
+++ R/wp_check_date_inputs.R
@@ -9,17 +9,24 @@
 #'   
 wp_check_date_inputs <- function(from, to){
   from <- as.character(from)
-  to <- as.character(to)
+  to   <- as.character(to)
+  # to small a value (no data before 2007)
   if ( wp_date(from) < wp_date("2007-12-01")  ) { 
     from <- wp_date("2007-12-01")
   } 
+  if ( wp_date(to) < wp_date("2007-12-01")  ) { 
+    to <- wp_date("2007-12-01")
+  } 
+  # to large a value (data beyond today)
   if ( wp_date(to) > Sys.Date()  ) { 
     to <- Sys.Date()
   } 
-  if ( wp_date(to) < wp_date(from) ) {
-    from.saved <- from
-    from <- to
-    to <- from.saved
+  if ( wp_date(from) > Sys.Date()  ) { 
+    from <- Sys.Date()
+  } 
+  # from larger than to
+  if ( wp_date(to) <= wp_date(from) ) {
+    error("In wp_check_date_inputs: from-date larger than to-date.")
   }
   return( list(from=from, to=to) )
 }

eaea17564ec86f0449d704c4a190fd926269980b
Peter Meissner <retep.meissner@gmail.com>
2015-05-20 14:16:45 +0200

    vignette

--- inst/doc/using-wikipediatrend.R
+++ inst/doc/using-wikipediatrend.R
@@ -111,14 +111,7 @@ ggplot(page_views, aes(x=date, y=count, group=lang, color=lang)) +
   scale_colour_brewer(palette="Set1") + 
   guides(colour = guide_legend(override.aes = list(alpha = 1)))
 
-## ---- message=F----------------------------------------------------------
-if ( !require(BreakoutDetection) ){    
-  devtools::install_github("twitter/BreakoutDetection")
-  library(BreakoutDetection)
-}
-library(dplyr)
-
-## ---- message=F----------------------------------------------------------
+## ---- include=F----------------------------------------------------------
 if ( !require(AnomalyDetection) ){    
   devtools::install_github("twitter/AnomalyDetection")
   library(AnomalyDetection)
@@ -126,8 +119,14 @@ if ( !require(AnomalyDetection) ){
 library(dplyr)
 library(ggplot2)
 
+## ---- eval=FALSE---------------------------------------------------------
+#  devtools::install_github("twitter/AnomalyDetection")
+#  library(AnomalyDetection)
+#  library(dplyr)
+#  library(ggplot2)
 
-page_views <- wp_trend("Wikipedia:Pageview_statistics", from = "2012-01-01")
+## ------------------------------------------------------------------------
+page_views <- wp_trend("Syria", from = "2012-01-01")
 
 page_views_br <- 
   page_views  %>% 
@@ -137,17 +136,113 @@ page_views_br <-
   as.data.frame() %>% 
   mutate(timestamp = as.POSIXct(timestamp))
 
-res <- AnomalyDetectionTs(page_views_br, plot=TRUE, longterm=T)$anoms
+## ------------------------------------------------------------------------
+res <- 
+  AnomalyDetectionTs(
+    x         = page_views_br, 
+    alpha     = 0.05, 
+    max_anoms = 0.40,
+    direction = "both",
+    longterm  = T
+  )$anoms
 res$timestamp <- as.Date(res$timestamp)
 
-ggplot( data=page_views, aes(x=date, y=count) ) + 
-  #geom_smooth(color="#4D87B740", fill="#4D87B740") +
-  geom_smooth(
-    data=filter(page_views, !(page_views$date %in% res$timestamp) ), 
-    aes(x=date, y=count), color="#EE000040", fill="#EE000040") +
-  geom_point(data=res, aes(x=timestamp, y=anoms),color="red2",size=3) +
-  theme_bw() + stat_smooth(level=1-1e-15)
+head(res)
+
+## ------------------------------------------------------------------------
+page_views <- 
+  page_views  %>% 
+  mutate(normal = !(page_views$date %in% res$timestamp))  %>% 
+  mutate(anom   =   page_views$date %in% res$timestamp )
+class(page_views) <- c("wp_df", "data.frame")
+
+## ---- message=FALSE------------------------------------------------------
+(
+p <-
+  ggplot( data=page_views, aes(x=date, y=count) ) + 
+    geom_line(color="steelblue") +
+    geom_point(data=filter(page_views, anom==T), color="red2", size=2) +
+    theme_bw()
+)
+
+## ---- message=FALSE------------------------------------------------------
+p + 
+  geom_line(stat = "smooth", size=2, color="red2", alpha=0.7) + 
+  geom_line(data=filter(page_views, anom==F), 
+            stat = "smooth", size=2, color="dodgerblue4", alpha=0.5) 
+
+## ------------------------------------------------------------------------
+page_views_clean <- 
+  page_views  %>% 
+  filter(anom==F)  %>% 
+  select(date, count, lang, page, rank, month, title)
+
+page_views_br_clean <- 
+  page_views_br  %>% 
+  filter(page_views$anom==F)
+
+## ---- include=FALSE------------------------------------------------------
+if ( !require(BreakoutDetection) ){    
+  devtools::install_github("twitter/BreakoutDetection")
+  library(BreakoutDetection)
+}
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  devtools::install_github("twitter/BreakoutDetection")
+#  library(BreakoutDetection)
+#  library(dplyr)
+#  library(ggplot2)
+#  library(magrittr)
+
+## ------------------------------------------------------------------------
+br <- 
+  breakout(
+    page_views_br_clean, 
+    min.size = 30, 
+    plot=T, 
+    method = 'multi', 
+    percent=0.05
+  )
+br
+
+## ------------------------------------------------------------------------
+breaks <- page_views_clean[br$loc,]
+breaks
 
 
+## ------------------------------------------------------------------------
+page_views_clean$span <- 0
+for (d in breaks$date ) {
+  page_views_clean$span[ page_views_clean$date > d ] %<>% add(1)
+}
 
+page_views_clean$mcount <- 0
+for (s in unique(page_views_clean$span) ) {
+  iffer <- page_views_clean$span == s
+  page_views_clean$mcount[ iffer ] <- mean(page_views_clean$count[iffer])
+}
+
+spans <- 
+  page_views_clean  %>% 
+    as_data_frame() %>% 
+    group_by(span) %>% 
+    summarize(
+      start      = min(date), 
+      end        = max(date), 
+      length     = end-start,
+      mean_count = round(mean(count)),
+      min_count  = min(count),
+      max_count  = max(count),
+      var_count  = var(count)
+    )
+spans
+
+## ------------------------------------------------------------------------
+ggplot(page_views_clean, aes(x=date, y=count) ) + 
+  geom_line(alpha=0.5) + 
+  geom_line(aes(y=mcount), alpha=0.5, color="red2", size=1.2) + 
+  theme_bw()
 

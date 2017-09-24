ee72fb22975354e4d8f2c3703a74199b1e1a70f3
petermeissner <retep.meissner@gmail.com>
2017-09-24 23:34:19 +0200

    tests for wp_trend()

---
+++ tests/testthat/test_wp_trend.R
@@ -0,0 +1,30 @@
+context("wp_trend()")
+# ----------------------------------------------------------
+
+test_that("wp_trend()", {
+  expect_silent(
+    wp_trend(page = "Test")
+  )
+  
+  expect_silent(
+   res <- wp_trend(page = "Test", from = "2016-01-01", to = "2016-01-01")
+  )
+  
+  expect_silent(
+    plot(res)
+  )
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+

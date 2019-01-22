933864259ad22487ebbdc0dd203e5717b0578012
Simon Munzert <simon.munzert@gmail.com>
2019-01-22 20:24:28 +0100

    Update zzz.R
    rectified typos

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
 .onAttach <- function(libname, pkgname){
   packageStartupMessage(
     "
-  [wikipedaitrend]
+  [wikipediatrend]
     
   Note:
     
     - Data before 2016-01-01 
       * is provided by petermeissner.de and
-      * was prepared in a project comissioned by the Hertie School of Governance (Simon Munzert)
+      * was prepared in a project commissioned by the Hertie School of Governance (Simon Munzert)
       * and supported by the Daimler and Benz Foundation.
     
     - Data from 2016-01-01 onwards 
